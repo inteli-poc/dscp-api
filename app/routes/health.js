91861a0578768cbb663c09baa61b8f1f8ec109dd
Darryl Morton <darryl.morton@digicatapult.org.uk>
2021-12-01 10:35:08 +0000

    api version returned via health check

--- app/routes/health.js
+++ app/routes/health.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const { API_VERSION } = require('../env')
+
 router.get('/', async (req, res) => {
-  res.status(200).json({ status: 'ok' })
+  res.status(200).json({ status: 'ok', version: API_VERSION })
 })
 
 module.exports = router
